7c8f149af4c1fc918679f98d6531da58f50a57b7
Tim Baumann <tim@timbaumann.info>
2015-12-20 10:54:50 +0100

    Gloss-Aufgabe

--- gloss-game/game.hs
+++ gloss-game/game.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import           Graphics.Gloss.Interface.Pure.Game
+import Graphics.Gloss.Interface.Pure.Game
 
 main :: IO ()
 main = play displayMode white fps initialState render handleInput step
@@ -11,28 +11,35 @@ main = play displayMode white fps initialState render handleInput step
         windowPos = (10, 10)
         fps = 100
 
+-- Speichere in dieser Datenstruktur den Zustand deines Spiels, also Positionen
+-- und Geschwindigkeit von deiner Spielfigur, den Gegnern, von sich bewegenden
+-- Plattformen, die Anzahl an verbleibenden <3 usw.
 data GameState = GameState
   { squarePosition :: Point -- ^ Point ist ein Alias für (Float, Float)
-  , picture        :: Picture
-      -- ^ Unter https://hackage.haskell.org/package/gloss-1.9.4.1/docs/Graphics-Gloss-Data-Picture.html findest
-      --   du eine Dokumentation aller Funktionen, mit denen du ein Picture produzieren kannst.
-      --   makeColor nimmt den Rot-, Grün-, Blau- und Transparenz-Anteil einer Farbe als Floats
-      --   zwischen 0 und 1.0.
   } deriving (Show)
 
+-- Der Zustand zu Beginn des Spiels.
 initialState :: GameState
 initialState = GameState
   { squarePosition = (0, 0)
-  , picture = color (makeColor 0.5 1.0 0.1 1.0) (rectangleSolid 100 40)
   }
 
--- Gloss rendert Pictures, wenn man nichts tut, bei (0, 0) also in der
--- Mitte des Bildschirms, darum müssen wir mit translate x y bild unser
--- Bild an die richtige Stelle verschieben
+-- Gloss rendert Pictures, wenn man nichts tut, bei (0, 0) also in der Mitte des
+-- Bildschirms, darum müssen wir mit translate x y bild unser das Bild an die
+-- richtige Stelle verschieben. Unter
+-- https://hackage.haskell.org/package/gloss-1.9.4.1/docs/
+-- Graphics-Gloss-Data-Picture.html findest du eine Dokumentation aller
+-- Funktionen, mit denen du ein Picture produzieren kannst. makeColor nimmt den
+-- Rot-, Grün-, Blau- und Transparenz-Anteil einer Farbe als Floats zwischen 0
+-- und 1.0.
 render :: GameState -> Picture
-render (GameState (x, y) pic)  = translate x y pic
+render (GameState (x, y) pic) =
+  translate x y pic $ color (makeColor 0.5 1.0 0.1 1.0) (rectangleSolid 100 40)
 
--- Unter https://hackage.haskell.org/package/gloss-1.9.4.1/docs/Graphics-Gloss-Interface-Pure-Game.html#t:Event
+-- In dieser Funktion beschreibsts du, wie sich der Zustand ändern soll , wenn
+-- der Benutzer die Maus bewegt, klickt oder eine Taste drückt.
+-- Unter https://hackage.haskell.org/package/gloss-1.9.4.1/docs/
+-- Graphics-Gloss-Interface-Pure-Game.html#t:Event
 -- findest du die Dokumentation zu Event.
 handleInput :: Event -> GameState -> GameState
 handleInput (EventKey (SpecialKey k) _ _ _) (GameState (x, y) pic)
@@ -44,7 +51,8 @@ handleInput (EventKey (SpecialKey k) _ _ _) (GameState (x, y) pic)
       _ -> GameState (x,y) pic
 handleInput _ gs = gs
 
--- Step bekommt im ersten Argument die Änderung der Zeit,
+-- Beschreibe, wie sich der Zustand ändert, wenn Zeit verstreicht.
+-- `step` bekommt im ersten Argument die Änderung der Zeit,
 -- seit dem das letzte Mal step aufgerufen wurde in Sekunden.
 step :: Float -> GameState -> GameState
-step _ gs = gs
+step _ gs = gs -- keine Zustandsänderung
